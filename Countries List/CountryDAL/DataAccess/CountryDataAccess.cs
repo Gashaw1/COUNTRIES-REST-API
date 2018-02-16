f723843afe004f85de844ff72ac985143e9efaca
Gashaw1 <akalc2004@yahoo.com>
2018-02-15 19:40:31 -0500

    .

---
+++ Countries List/CountryDAL/DataAccess/CountryDataAccess.cs
@@ -0,0 +1,35 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace CountryDAL.DataAccess
+{
+    public class CountryDataAccess
+    {
+
+        private string cs = ConfigurationManager.ConnectionStrings[""].ConnectionString;
+
+        //return all users
+        DataSet dataset { get; set; }
+        public DataSet returnCountrys()
+        {
+
+            dataset = new DataSet();
+            using (SqlConnection con = new SqlConnection(cs))
+            {
+
+                SqlCommand cmd = new SqlCommand("", con);
+
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                SqlDataAdapter dataadapter = new SqlDataAdapter(cmd);
+
+                dataadapter.Fill(dataset);
+
+                return dataset;
+
+            }
+
+        }
+    }
+}
