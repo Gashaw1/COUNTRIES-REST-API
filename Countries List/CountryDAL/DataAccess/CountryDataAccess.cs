95c6942b918c7169eb76822d743695c7f2be5d87
Gashaw1 <akalc2004@yahoo.com>
2018-02-18 20:46:47 -0500

    .

--- Countries List/CountryDAL/DataAccess/CountryDataAccess.cs
+++ Countries List/CountryDAL/DataAccess/CountryDataAccess.cs
@@ -9,9 +9,9 @@ namespace CountryDAL.DataAccess
 
         private string cs = ConfigurationManager.ConnectionStrings[""].ConnectionString;
 
-        //return all users
+        //return all country
         DataSet dataset { get; set; }
-        public DataSet returnCountrys()
+        public DataSet returnCountry()
         {
 
             dataset = new DataSet();
@@ -28,6 +28,28 @@ namespace CountryDAL.DataAccess
 
                 return dataset;
 
+            }
+
+        }
+        //return country by id
+        public DataSet returnCountry(int id)
+        {
+
+            dataset = new DataSet();
+            using (SqlConnection con = new SqlConnection(cs))
+            {
+                SqlCommand cmd = new SqlCommand("", con);
+
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                SqlParameter param = new SqlParameter("", id);  
+                cmd.Parameters.Add(param);               
+
+                SqlDataAdapter dataadapter = new SqlDataAdapter(cmd);
+
+                dataadapter.Fill(dataset);
+
+                return dataset;
             }
 
         }
