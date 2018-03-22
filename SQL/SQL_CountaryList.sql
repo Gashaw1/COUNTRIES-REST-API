75f9f88d09ac192e71ed5ce14dc6217708ff35d6
Gashaw1 <akalc2004@yahoo.com>
2018-03-22 15:16:57 -0400

    .

--- SQL/SQL_CountaryList.sql
+++ SQL/SQL_CountaryList.sql
@@ -1,8 +1,7 @@
 Create Table Continent
 (
 	   CotinentID nvarchar(50),
-       CotinentName nvarchar(50),
-       CotinentPopulation int,
+       CotinentName nvarchar(50)      
 			  CONSTRAINT CContinent_CotinentID_PK PRIMARY KEY(CotinentID)
       
 )
@@ -20,4 +19,45 @@ Create Table Countary
 		
 )
 go
+
+create proc SP_ReturnContent
+as
+begin
+	select * from Continent;
+end
+go
+
+create proc SP_ReturnContentByID 
+@CotinentID nvarchar(50)
+as
+begin
+	select * from Continent
+	where Continent.CotinentID = @CotinentID;
+end
+
+go
+Create proc Sp_ReturnCountary
+as
+begin
+	select * from Countary;
+end
+
+go
+Create proc Sp_ReturnCountaryByID
+@countryID nvarchar(50)
+as
+begin
+	select * from Countary
+	where countryID = @countryID
+end
+go
+--sample data
+insert into Continent values('2dududfu22222','Aferica')
+insert into Continent values('e8e8e8e8e88e','Asia')
+
+Insert into Countary values('39diuieiewspo','Ethiopia','addis ababa','9999999','2dududfu22222')
+Insert into Countary values('39dwweiewspo','Gana','Dakar','799999','2dududfu22222')
+
+Insert into Countary values('wwwww0w0ww00w','Japan','Beging','4444444','e8e8e8e8e88e')
+Insert into Countary values('wwwwwww666sw6','China','Tokio','9333999','e8e8e8e8e88e')
 
